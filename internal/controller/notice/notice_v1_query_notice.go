65153762aaa2b54ce865262d04891de8953ac013
chad.wang <chad.wang@icloudsky.com>
2024-02-20 17:57:07 +0800

    imp query notice

--- internal/controller/notice/notice_v1_query_notice.go
+++ internal/controller/notice/notice_v1_query_notice.go
@@ -2,13 +2,23 @@ package notice
 
 import (
 	"context"
-
+	"delay-notice/internal/dao"
+	"delay-notice/internal/model/entity"
 	"github.com/gogf/gf/v2/errors/gcode"
 	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/marcellowy/go-common/gogf/vlog"
 
 	"delay-notice/api/notice/v1"
 )
 
 func (c *ControllerV1) QueryNotice(ctx context.Context, req *v1.QueryNoticeReq) (res *v1.QueryNoticeRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	res = &v1.QueryNoticeRes{}
+	var noticeRow = entity.Notice{}
+	if err = dao.DB.First(&noticeRow).Error; err != nil {
+		vlog.Error(ctx, err)
+		return nil, gerror.NewCode(gcode.CodeDbOperationError)
+	}
+	res.Status = v1.NoticeStatus(noticeRow.Status)
+	res.Message = noticeRow.StatusDesc
+	return
 }
